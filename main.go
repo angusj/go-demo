44a769c3eb4dd10fd3a8fd8ab8c9970dc0aaf222
Yuanjie Wang <yuanjie.wang@visionz.com>
2021-04-26 10:45:02 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+	"os"
+)
+
+func main() {
+	db, err := sql.Open("mysql",
+		"root:root@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		fmt.Printf("Original error: %T %v\n", errors.Cause(err), errors.Cause(err))
+		fmt.Printf("Stack trace:\n%+v\n", err)
+		os.Exit(1)
+	}
+	type row struct {
+		age  int
+		name string
+	}
+	var r row
+	err = db.QueryRow("select age, name from users where age = 2").Scan(&r.age, &r.name)
+	if err != nil {
+		fmt.Printf("Original error: %T %v\n", errors.Cause(err), errors.Cause(err))
+		fmt.Printf("Stack trace:\n%+v\n", err)
+		os.Exit(1)
+	}
+
+	//rows, err := db.Query("select age, name from users where age = 2")
+	//if err != nil {
+	//	fmt.Printf("Original error: %T %v\n", errors.Cause(err), errors.Cause(err))
+	//	fmt.Printf("Stack trace:\n%+v\n", err)
+	//	os.Exit(1)
+	//}
+	//defer rows.Close()
+	//type row struct {
+	//	age  int
+	//	name string
+	//}
+	//got := []row{}
+	//for rows.Next() {
+	//	var r row
+	//	err = rows.Scan(&r.age, &r.name)
+	//	if err != nil {
+	//		fmt.Printf("Original error: %T %v\n", errors.Cause(err), errors.Cause(err))
+	//		fmt.Printf("Stack trace:\n%+v\n", err)
+	//		os.Exit(1)
+	//	}
+	//	got = append(got, r)
+	//}
+	//fmt.Printf("got:%v\n", got)
+}
